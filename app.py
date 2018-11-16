5373b8d48252e92445b8db164efa2466ac958401
Chiranjivi Jawale <cj@wunderfund.co>
2018-11-16 10:58:27 -0800

    add success.html

--- app.py
+++ app.py
@@ -8,25 +8,46 @@ import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = os.path.basename('image')
+UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
+
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['xml']
+    file = request.files['image']
     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
     # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
     file.save(f)
-    parseXML(f)
-    return render_template('index.html')
+    # parseXML(f)
+    return render_template('success.html')
 
-# def parseXML(xmlfile):
-
+def parseXML(xmlfile):
+    # create element tree object
+    tree = ET.parse(xmlfile)
+    # get root element
+    root = tree.getroot()
+    # create empty list for news items
+    newsitems = []
+    # iterate news items
+    for item in root.findall('./test1'):
+        # empty news dictionary
+        news = {}
+        # iterate child elements of item
+        for child in item:
+            # special checking for namespace object content:media
+            if child.tag == 'color':
+                news['color'] = child.attrib['url']
+            else:
+                news[child.tag] = child.text.encode('utf8')
+        # append news dictionary to news items list
+        newsitems.append(news)
+    # return news items list
+    return newsitems
 
 if __name__ == '__main__':
    app.run(debug = True)
