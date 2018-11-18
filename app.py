275f30ad0cdd0181d79d56a54767af9699cf6da6
Chiranjivi Jawale <cj@wunderfund.co>
2018-11-18 00:37:39 -0800

    added GET request which returns the output file

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import glob
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from bs4 import BeautifulSoup
 import csv
 import xml.etree.cElementTree as ET
 import re
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 # app = Flask(__name__, instance_relative_config=True)
 
 SAVE_FOLDER = os.path.basename('savedFiles')
@@ -35,40 +36,39 @@ def upload_file():
     # m = re.search("Attorneys for Plaintiff (\w+) (\w+)", clean_data)
     clean_data = re.sub(',',' ',clean_data)
     clean_data = re.sub(' +',' ',clean_data)
-    m = re.search("(\w+) (\w+) an individual",clean_data)
+    m = re.search("FOR THE COUNTY OF (\w+) (\w+) (\w+)",clean_data)
     if m is None:
-        m = re.search("Attorneys for Plaintiff (\w+) (\w+)",clean_data)
+        m = re.search("(\w+) (\w+) an individual",clean_data)
+    # else:
+    #     m = re.search("Attorneys for Plaintiff (\w+) (\w+)",clean_data)
+    # else:
+    #     m = re.search("FOR THE COUNTY OF (\w+) (\w+) (\W+)", clean_data)
     print("Plaintiff is: " + m.group(1) + " " + m.group(2))
 
-    # result = re.search('vs.(.*)Defendants', clean_data)
-    # print("Defendent is:" + result.group(1)) 
+    result = re.search('vs.(.*)Defendants', clean_data)
+    if result is None:
+        result = re.search('v.(.*)j Defendants.', clean_data)
+    print("Defendent is:" + result.group(1))
 
     # save to file
+    file_name = os.path.splitext(os.path.basename(f))[0]
+    print(file_name)
 
-    # new_file = open('file.txt', 'w') #open the file(this will not only open the file also
-    # #if you had one will create a new one on top or it would create one if you
-    # #didn't have one
-    # new_file.write("Plaintiff is: " + m.group(1) + " " + m.group(2) + "\n \nDefendent is:" + result.group(1)) #this will put the info in the file
-    # new_file.close()
-    #parseXML()
+
+    parsed_file = open(SAVE_FOLDER + '/' + file_name + '.txt', 'w') #open the file(this will not only open the file also
+    #if you had one will create a new one on top or it would create one if you
+    #didn't have one
+    parsed_file.write("Plaintiff is: " + m.group(1) + " " + m.group(2) + "\n \nDefendent is:" + result.group(1)) #this will put the info in the file
+    parsed_file.close()
     return render_template('success.html')
 
 def remove_tags(text):
     TAG_RE = re.compile(r'<[^>]+>')
     return TAG_RE.sub('', text)
 
-# def parseXML():
-#     dir_path = UPLOAD_FOLDER
-#     files = [ ]
-#     f = open(dir_path)
-#     for file in files:  # add files to the message
-#         file_path = os.path.join(dir_path, file)
-#         print(file_path)
-#         tree = ET.ElementTree(file = file_path)
-#         root = tree.getroot()
-#         for data in root:
-#             # if(data.tag=='plane')
-#             return child.get('color')
+@app.route('/getfile', methods=['GET'])
+def get_file():
+    return (SAVE_FOLDER)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
